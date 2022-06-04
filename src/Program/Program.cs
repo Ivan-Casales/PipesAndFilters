542d404f68ee1aeb6b1efc079de7117623b35eb7
Ivan Casales <casalesivan@hotmail.com>
2022-06-04 14:44:46 -0300

    Primer parte

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"../Program/beer.jpg");
+
+            PipeNull pipenull = new PipeNull();
+            FilterNegative filterNegative = new FilterNegative();
+            PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipenull);
+
+            FilterGreyscale filterGreyscale = new FilterGreyscale();
+            PipeSerial pipeSerialGrey = new PipeSerial(filterGreyscale, pipeSerialNegative);
+
+            picture = pipeSerialGrey.Send(picture);
+
+            provider.SavePicture(picture, @"../Program/Saved1.jpg");
         }
     }
 }
